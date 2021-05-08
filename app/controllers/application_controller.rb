0e6fe9c73fc9d3da79ac5e238e2a2ff6567eace6
github username <schacter.jon@gmail.com>
2021-05-07 18:42:27 -0700

    remove forgery protection macro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    include ActionController::RequestForgeryProtection
-
-    protect_from_forgery with: :exception
-
+    
     def current_user
         User.find_by(id: session[:user_id])
     end
