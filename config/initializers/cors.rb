21566beddb11f62049e6776880be7fa9da62b953
github username <schacter.jon@gmail.com>
2021-05-07 19:50:27 -0700

    change frontend to https

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000', 'http://jschacter-shoppies.herokuapp.com'
+        origins 'http://localhost:3000', 'https://jschacter-shoppies.herokuapp.com'
     
         resource '*',
           headers: :any,
